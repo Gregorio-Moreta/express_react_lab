0453eaaf10c8131b98da3b2687c6ea4b37a6a024
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-10-30 12:04:02 -0400

    Backend is done

---
+++ server.js
@@ -0,0 +1,35 @@
+//  Import Dependencies
+const express = require("express");
+const cors = require("cors");
+
+//  Import Json files 
+const projects = require("./projects.json");
+const about = require("./about.json");
+
+//  Create our app object
+const app = express();
+
+//  Set up middleware
+app.use(cors());
+
+//  Home route for testing our app
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+//  Route for retrieving projects
+app.get("/projects", (req, res) => {
+    res.json(about);
+});
+
+//  Route for retrievign about information
+app.get("/about", (req, res) => {
+    //  Send projects via JSON
+    res.json(about);
+});
+
+//  Declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+//  Turn on the server listener
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
